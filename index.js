0af16ee1db749f54c8f786a3d4fe401a27d18ff1
Nathan Nichols <nathann@ori.org>
2016-10-30 21:24:13 -0700

    heroku & postgres

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({ port: (process.env.PORT || 3000) });
 
 server.route({
     method: 'GET',
@@ -27,4 +27,21 @@ server.start((err) => {
         throw err;
     }
     console.log(`Server running at: ${server.info.uri}`);
+});
+
+// Some postgres goodness. 
+// https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
+
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
 });
