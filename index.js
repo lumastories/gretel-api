45706938565c525cd6c0e8e6b5f539ad950f2e66
Nathan Nichols <natsamnic@gmail.com>
2016-10-30 21:52:36 -0700

    Revert DB changes

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 'use strict';
 
 const Hapi = require('hapi');
-const pg = require('pg');
-pg.defaults.ssl = true;
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
 
 server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply('Hello, world!');
-  }
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
 });
 
 server.route({
-  method: 'GET',
-  path: '/{name}',
-  handler: function (request, reply) {
-    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/db',
-  handler: function (request, reply) {
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
-      if (err) throw err;
-      client.query('SELECT table_schema, table_name FROM information_schema.tables;')
-      .on('row', function(row) {
-        reply(JSON.stringify(row));
-      });
-    });
-  }
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
 });
 
 server.start((err) => {
-  if (err) throw err;
-  console.log(`Server running at: ${server.info.uri}`);
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
 });
 
+// Some postgres goodness. 
+// https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
